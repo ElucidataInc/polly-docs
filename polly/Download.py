e1ac9a81b53ea932993e9a854dbfa44a9893634b
shreya-el <115142535+shreya-el@users.noreply.github.com>
2022-12-20 16:57:25 +0530

    Update Download.py

--- polly/Download.py
+++ polly/Download.py
@@ -5,14 +5,11 @@ The OmixAtlas class contains functions to download gct, h5ad, and vcf files, and
     def download_data(self, repo_id, _id: str):
         """
         To download any dataset, the following function can be used to get the signed URL of the dataset.
-        
+        The data can be downloaded by clicking on this URL. 
+        NOTE: This signed URL expires after 60 minutes from when it is generated.
+         
         The repo_name OR repo_id of an OmixAtlas can be identified by calling the get_all_omixatlas() function.
         The dataset_id can be obtained by querying the metadata at the dataset level using query_metadata().
-        
-        The output of this function is a *signed URL*. The data can be downloaded by clicking on this URL.
-        NOTE: This signed URL expires after 60 minutes from when it is generated.
-        
-        The output data is in gct/h5ad/vcf/mmcif format.
         This data can be parsed into a data frame for better accessibility using the code under the examples section.
 
         Args:
